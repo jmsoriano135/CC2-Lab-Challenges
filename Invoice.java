a55bf9f9c33e9fb80f062b831fe245a2eb925594
jmsoriano135 <sorianojm135@gmail.com>
2024-12-12 17:51:15 +0800

    Add files via upload

---
+++ Invoice.java
@@ -0,0 +1,15 @@
+public class Invoice {
+    public static void main(String[] args) {
+        System.out.println("JM Cycling Goods \"Cheap Prices\"");
+        System.out.println("27 Reriet Street");
+
+        System.out.println("Item\t\tPrice");
+        System.out.println("----------------------");
+        System.out.println("Shimano RC5 Cleat Shoes\t$50.00");
+        System.out.println("Kask Protone Helmet\t\t$15.00");
+        System.out.println("Tri-Suit Jersey\t\t\t$10.00");
+        
+        System.out.println("\nTotal Amount Due: $75.00");
+        System.out.println("\nThank you for your purchase!");
+    }
+}
