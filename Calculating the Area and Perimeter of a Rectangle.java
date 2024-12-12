2460744d294ce98dbf26e2ba70c1615cb5abac2a
jmsoriano135 <sorianojm135@gmail.com>
2024-12-12 17:52:07 +0800

    Add files via upload

---
+++ Calculating the Area and Perimeter of a Rectangle.java
@@ -0,0 +1,24 @@
+package jmsoriano2;
+
+public class jmsoriano2 {
+
+	public static void main(String[] args) {
+	
+		        // Given values
+		        double length = 10.0; // Replace with the actual length in meters
+		        double width = 5.0;  // Replace with the actual width in meters
+		        
+		        // Calculations
+		        double area = length * width;
+		        double perimeter = 2 * (length + width);
+		        
+		        // Output
+		        System.out.println("The length of the garden is: " + length + " meters");
+		        System.out.println("The width of the garden is: " + width + " meters");
+		        System.out.println("The area of the garden is: " + area + " square meters");
+		        System.out.println("The perimeter of the garden is: " + perimeter + " meters");
+		    
+		
+	}
+
+}
